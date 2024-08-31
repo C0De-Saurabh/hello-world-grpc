6112ebf2ce58055f9c0d13bfef4618350a54096c
c0de-saurabh <saurabhkumarw.990sk@gmail.com>
2024-08-31 21:24:42 +0530

    fixed Deprecated grpc.Dial and fixed calling with insecure-TLS

--- client.go
+++ client.go
@@ -8,10 +8,12 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	//"google.golang.org/grpc/credentials/insecure"
 )
 
 func main() {
-	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials())
+	conn, err := grpc.NewClient("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("Failed to connect: %v", err)
 	}
